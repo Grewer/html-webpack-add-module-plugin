3af5723071a30de3755bab66df482c6411d05b92
Grewer <grewer@grewer.cn>
2018-06-28 11:14:09 +0800

    feat(module): add and update readme

--- README.md
+++ README.md
@@ -1,6 +1,24 @@
 ## htmlWebpackAddModulePlugin
 
+> add script attribute type module or nomodule
+
 ### Usage
+
 ```
-//todo 
+npm i html-webpack-add-module-plugin -S
 ```
+
+```
+// webpack.conf.js
+const htmlWebpackAddModulePlugin = require('html-webpack-add-module-plugin')
+
+
+plugin:{
+	new htmlWebpackAddModulePlugin({
+      module: '', //  if key in src, change type attribute to module, if `module:'all'` , change all
+      nomodule:'' //  same as above , add nomodule attribute
+    }),
+}
+```
+
+
--- src/index.js
+++ src/index.js
@@ -1,47 +1,55 @@
 function htmlWebpackAddModulePlugin(options) {
-  // Configure your plugin with options...
-  // options key
+  this.module = options.module
+  this.nomodule = options.nomodule
 }
 
+
 htmlWebpackAddModulePlugin.prototype.apply = function (compiler) {
-  // console.log(compiler)
-  var self = this;
+
+  function addModule(htmlPluginData, callback) {
+    if (this.module) {
+      changeAttributes.call(this, htmlPluginData, this.module === 'all' ? false : 'module', 'type', 'module')
+    }
+
+    if (this.nomodule) {
+      if (this.module !== 'all' && this.nomodule === 'all') {
+        changeAttributes.call(this, htmlPluginData, false, 'nomodule', true)
+      } else {
+        changeAttributes.call(this, htmlPluginData, 'nomodule', 'nomodule', true)
+      }
+    }
+    callback(null, htmlPluginData)
+  }
+
+  function changeAttributes(htmlData, condition, key, value) {
+    htmlData.body.forEach(i => {
+      if (condition) {
+        if (i.attributes.src.indexOf(this[condition]) > -1) {
+          delete i.attributes['type']
+          i.attributes[key] = value
+        }
+      } else {
+        delete i.attributes['type']
+        i.attributes[key] = value
+      }
+    })
+  }
+
+  var self = this
 
   if (compiler.hooks) {
-    console.log('run1')
     // webpack 4 support
     compiler.hooks.compilation.tap('htmlWebpackAddModulePlugin', (compilation) => {
-      console.log('The compiler is starting a new compilation...');
-
-      compilation.hooks.htmlWebpackPluginAfterHtmlProcessing.tapAsync(
-        'htmlWebpackAddModulePlugin',
-        (data, cb) => {
-          data.html += 'The Magic Footer'
-          console.log(data)
-          cb(null, data)
-        }
+      compilation.hooks.htmlWebpackPluginAlterAssetTags.tapAsync(
+        'htmlWebpackAddModulePlugin', addModule.bind(self)
       )
     })
   } else {
-    console.log('run2')
-    // Hook into the html-webpack-plugin processing
-    compiler.plugin('compilation', function (compilation) {
-      compilation.plugin('html-webpack-plugin-alter-asset-tags', function (htmlPluginData, callback) {
-        console.log('tags')
-        console.log(htmlPluginData.body)
-
-        // let newArr = htmlPluginData.body.map(i => {
-        //   i.attributes.noModule = true
-        // })
-        htmlPluginData.body.forEach(i => {
-          console.log(i,i.type = 'module')
-            i.attributes.type = 'module'
-        })
-        // htmlPluginData.body = [...htmlPluginData.body,...newArr]
-
-        callback(null, htmlPluginData)
-      });
+    compiler.plugin('compilation', (compilation) => {
+      compilation.plugin('html-webpack-plugin-alter-asset-tags', addModule.bind(self));
     });
   }
 
 }
+
+module.exports = htmlWebpackAddModulePlugin
